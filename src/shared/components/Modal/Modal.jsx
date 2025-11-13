8afffbb5b61ce3aee25a846c30ed86d955826f60
Marina Nazina <nzyame@gmail.com>
2025-11-13 17:56:29 +0000

    some shared components

---
+++ src/shared/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import Modal from 'react-modal';
+import css from './Modal.module.css';
+import { baseStyles } from './ModalStyles';
+import Icon from '../Icon/Icon';
+
+Modal.setAppElement('#root');
+
+export default function CustomModal({
+  isOpen,
+  onRequestClose,
+  title,
+  component: Component,
+  componentProps,
+  additionalStyles = {},
+}) {
+  const mergedStyles = {
+    content: { ...baseStyles.content, ...additionalStyles.content },
+    overlay: { ...baseStyles.overlay, ...additionalStyles.overlay },
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={mergedStyles}
+      contentLabel={title}
+    >
+      <button onClick={onRequestClose} className={css.closeButton}>
+        <Icon className={css.iClose} id="i-close" width={18} height={18} />
+      </button>
+      <h1 style={{ display: 'none' }}>{title}</h1>
+      {Component && <Component {...componentProps} />}
+    </Modal>
+  );
+}
---
+++ src/shared/components/Modal/ModalStyles.js
@@ -0,0 +1,21 @@
+export const baseStyles = {
+  content: {
+    width: '',
+    height: '',
+    borderRadius: '20px',
+    padding: '40px',
+    backgroundColor: 'var(--white)',
+    border: 'none',
+    outline: 'transparent',
+    position: 'relative',
+    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+    overflow: 'auto',
+  },
+
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
