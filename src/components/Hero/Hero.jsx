8276901135616e450a2507eae22c88093f3610f6
Marina Nazina <nzyame@gmail.com>
2025-11-18 14:04:00 +0000

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,52 +7,21 @@ export default function Hero() {
     console.log('Primary button clicked!');
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted!');
-  };
-
-  const handleDefaultClick = () => {
-    console.log('Default click');
-  };
-
-  const handleLinkClick = () => {
-    console.log('Link button clicked!');
-  };
-
   return (
     <section className={css.hero}>
       <Container>
         <div className={css.hero__wrapper}>
           <h1 className={css.title}>HANDCRAFTED LAMPS & FURNITURE</h1>
-          <p className={css.description}>
-            Handmade lamps and furniture from pipes or old cameras with Edison
-            bulbs
-          </p>
+          <div className={css.hero__description_wrapper}>
+            <p className={css.description}>
+              Handmade lamps and furniture from pipes or old cameras with Edison
+              bulbs
+            </p>
+            <Button variant="primary" onClick={handleClick}>
+              View our works
+            </Button>
+          </div>
         </div>
-
-        {/* Primary (звичайна кнопка) */}
-        <Button variant="primary" onClick={handleClick}>
-          Click me
-        </Button>
-
-        <form onSubmit={handleSubmit}>
-          <Button variant="primary" type="submit">
-            Submit form
-          </Button>
-        </form>
-
-        <Button variant="default" onClick={handleDefaultClick}>
-          Default
-        </Button>
-
-        <Button variant="link" onClick={handleLinkClick}>
-          Go to page
-        </Button>
-
-        <Button variant="link" active={true} onClick={handleDefaultClick}>
-          Default
-        </Button>
       </Container>
     </section>
   );
