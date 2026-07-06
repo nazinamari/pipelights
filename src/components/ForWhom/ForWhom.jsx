610c731f007b942bd49b2c3836d1d78637c21cbc
Maryna Nazina <nzyame@gmail.com>
2026-07-06 14:46:05 +0100

    update

---
+++ src/components/ForWhom/ForWhom.jsx
@@ -0,0 +1,3 @@
+export default function ForWhom() {
+  return <h1>Whor whom</h1>;
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,11 @@
 import Hero from '../../components/Hero/Hero';
+import ForWhom from '../../components/ForWhom/ForWhom';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <ForWhom />
+    </>
+  );
 }
